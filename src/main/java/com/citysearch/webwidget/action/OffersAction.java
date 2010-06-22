70e3ebf9b1d9adf3e5808638ead6ee3a5a6a791f
mandar <mandar.j@xiova.com>
2010-06-21 17:15:07 -0700

    offers action

--- src/main/java/com/citysearch/webwidget/action/OffersAction.java
+++ src/main/java/com/citysearch/webwidget/action/OffersAction.java
@@ -1,8 +1,7 @@
 package com.citysearch.webwidget.action;
 
+import java.util.Iterator;
 import java.util.List;
-import java.util.Properties;
-
 import org.apache.log4j.Logger;
 
 import com.citysearch.webwidget.bean.Offer;
@@ -10,26 +9,24 @@ import com.citysearch.webwidget.bean.OffersRequest;
 import com.citysearch.webwidget.exception.CitysearchException;
 import com.citysearch.webwidget.exception.InvalidRequestParametersException;
 import com.citysearch.webwidget.helper.OffersHelper;
-import com.citysearch.webwidget.util.PropertiesLoader;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ModelDriven;
 
 public class OffersAction extends AbstractCitySearchAction implements ModelDriven<OffersRequest> {
-
+	
     private Logger log = Logger.getLogger(getClass());
     private OffersRequest offersRequest = new OffersRequest();
     private List<Offer> offersList;
-    private static final String HASH = "#";
     private static final String CLICK_TRACKING_URL = "http://ad.doubleclick.net/clk;225291110;48835962;h?";
 
-    public List<Offer> getOffersList() {
-        return offersList;
-    }
+	public List<Offer> getOffersList() {
+		return offersList;
+	}
 
-    public void setOffersList(List<Offer> offersList) {
-        this.offersList = offersList;
-    }
-
+	public void setOffersList(List<Offer> offersList) {
+		this.offersList = offersList;
+	}
+	
     public OffersRequest getOffersRequest() {
         return offersRequest;
     }
@@ -41,39 +38,41 @@ public class OffersAction extends AbstractCitySearchAction implements ModelDrive
     public OffersRequest getModel() {
         return offersRequest;
     }
-
+    
     /**
-     * Calls the getoffers() method from offersHelper class to get the offers, review count is
-     * fetched for each offer by passing its listing id to profile API
+     * Calls the getoffers() method from offersHelper class to get the offers, review count 
+     * is fetched for each offer by passing its listing id to profile API
      * 
      * @return String
      * @throws CitysearchException
      */
     public String execute() throws CitysearchException {
-        log.info("=========Start offersAction execute()============================ >");
-        Properties apiProperties = PropertiesLoader.getAPIProperties();
-        OffersHelper helper = new OffersHelper(getResourceRootPath());
-        try {
-            offersList = helper.getOffers(offersRequest);
-            if (offersList == null || offersList.isEmpty()) {
-                log.info("Returning backfill from offer");
-                return "backfill";
-            }
-            for (Offer offer : offersList) {
-                String listingUrl = null;
-                if (!offer.getProfileUrl().equals(HASH)) {
-                    listingUrl = getTrackingUrl(offer.getProfileUrl(), CLICK_TRACKING_URL,
-                            offer.getListingId(), offersRequest.getPublisher(),
-                            offersRequest.getAdUnitName(), offersRequest.getAdUnitSize());
-                } else {
-                    listingUrl = HASH;
-                }
-                offer.setProfileUrl(listingUrl);
-            }
-        } catch (InvalidRequestParametersException ihre) {
-            log.error(ihre.getDetailedMessage());
+        log.info("=========Start offersAction execute()============================ >"); 
+        OffersHelper helper = new OffersHelper(getResourceRootPath());        
+        try{
+        	offersList = (List<Offer>) helper.getOffers(offersRequest);        	
+    	    if (offersList.size() == 0) {
+    	    	 log.info("Returning backfill from offer");
+                 return "backfill";                 
+    	    }   
+    	    Iterator<Offer> it = offersList.iterator();
+    	    while(it.hasNext()){
+    	    	 Offer offer = (Offer)it.next();
+        		 String listingUrl = null;        		  
+        		 if(offer.getProfileUrl() != null){
+                     listingUrl = getTrackingUrl(offer.getProfileUrl(),
+                    		 CLICK_TRACKING_URL,
+                             offer.getListingId(), offersRequest.getPublisher(),
+                             offersRequest.getAdUnitName(),offersRequest.getAdUnitSize());
+        		 }else{
+        			 listingUrl = "";
+        		 }
+                 offer.setProfileUrl(listingUrl);       	    	
+    	    }
+        }catch(InvalidRequestParametersException ihre){
+        	log.error(ihre.getDetailedMessage());
             throw ihre;
-        } catch (CitysearchException cse) {
+        }catch (CitysearchException cse){
             log.error(cse.getMessage());
             throw cse;
         }
