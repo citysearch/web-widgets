9a581aeab0e8216d2ff90b0258907778ba3980b7
Aspert Benjamin <aspert@aspert-laptop.(none)>
2010-09-01 14:50:11 -0700

    Fix to use correct field properties

--- src/main/java/com/citysearch/webwidget/facade/UrbanSpoonFacade.java
+++ src/main/java/com/citysearch/webwidget/facade/UrbanSpoonFacade.java
@@ -13,6 +13,7 @@ import com.citysearch.webwidget.bean.Offer;
 import com.citysearch.webwidget.bean.RequestBean;
 import com.citysearch.webwidget.exception.CitysearchException;
 import com.citysearch.webwidget.exception.InvalidRequestParametersException;
+import com.citysearch.webwidget.util.CommonConstants;
 
 public class UrbanSpoonFacade extends AbstractGrouponOffersFacade {
     private Logger log = Logger.getLogger(getClass());
@@ -44,11 +45,15 @@ public class UrbanSpoonFacade extends AbstractGrouponOffersFacade {
             dealsResponse.setHouseAds(nearbyResponse.getHouseAds());
 
         } else {
+            //Set the adunit name to offer so that we use the correct field properites.
+            request.setAdUnitName(CommonConstants.AD_UNIT_NAME_OFFERS);
             AbstractOffersFacade facade = new ConquestOffersFacade(contextPath, 1);
             List<Offer> offers = facade.getOffers(request);
             if (offers != null && !offers.isEmpty()) {
                 dealsResponse.setCitySearchOffer(offers.get(0));
             }
+            //Revert it back so that 1x1 are generated properly.
+            request.setAdUnitName(CommonConstants.AD_UNIT_NAME_DEALS);
         }
         return dealsResponse;
     }
