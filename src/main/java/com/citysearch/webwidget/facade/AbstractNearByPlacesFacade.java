5e230aadae92881a191a80034100dea7343f1a9a
Aspert Benjamin <aspert@aspert-laptop.(none)>
2010-08-31 23:15:22 -0700

    cleanup

--- src/main/java/com/citysearch/webwidget/facade/AbstractNearByPlacesFacade.java
+++ src/main/java/com/citysearch/webwidget/facade/AbstractNearByPlacesFacade.java
@@ -1,6 +1,7 @@
 package com.citysearch.webwidget.facade;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -18,91 +19,90 @@ import com.citysearch.webwidget.facade.helper.NearbyPlacesHelper;
 import com.citysearch.webwidget.facade.helper.SearchHelper;
 
 public abstract class AbstractNearByPlacesFacade {
-	private Logger log = Logger.getLogger(getClass());
-	protected String contextPath;
-	protected int displaySize;
-	protected PFPProxy pfpProxy;
+    private Logger log = Logger.getLogger(getClass());
+    protected String contextPath;
+    protected int displaySize;
+    protected PFPProxy pfpProxy;
 
-	protected AbstractNearByPlacesFacade(String contextPath, int displaySize) {
-		this.contextPath = contextPath;
-		this.displaySize = displaySize;
-		this.pfpProxy = new PFPProxy();
-	}
+    protected AbstractNearByPlacesFacade(String contextPath, int displaySize) {
+        this.contextPath = contextPath;
+        this.displaySize = displaySize;
+        this.pfpProxy = new PFPProxy();
+    }
 
-	public String getContextPath() {
-		return contextPath;
-	}
+    public String getContextPath() {
+        return contextPath;
+    }
 
-	public void setContextPath(String contextPath) {
-		this.contextPath = contextPath;
-	}
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
 
-	public int getDisplaySize() {
-		return displaySize;
-	}
+    public int getDisplaySize() {
+        return displaySize;
+    }
 
-	public void setDisplaySize(int displaySize) {
-		this.displaySize = displaySize;
-	}
+    public void setDisplaySize(int displaySize) {
+        this.displaySize = displaySize;
+    }
 
-	public abstract NearbyPlacesResponse getNearbyPlaces(RequestBean request)
-			throws InvalidRequestParametersException, CitysearchException;
+    public abstract NearbyPlacesResponse getNearbyPlaces(RequestBean request)
+            throws InvalidRequestParametersException, CitysearchException;
 
-	private List<NearbyPlace> toNearbyPlaces(RequestBean request,
-			List<PFPAd> ads) throws CitysearchException {
-		List<NearbyPlace> nearbyPlaces = null;
-		if (ads != null && !ads.isEmpty()) {
-			nearbyPlaces = new ArrayList<NearbyPlace>();
-			for (PFPAd ad : ads) {
-				nearbyPlaces.add(NearbyPlacesHelper.toNearbyPlace(request, ad));
-			}
-			NearbyPlacesHelper.addDefaultImages(nearbyPlaces, contextPath);
-		}
-		return nearbyPlaces;
-	}
+    private List<NearbyPlace> toNearbyPlaces(RequestBean request, List<PFPAd> ads)
+            throws CitysearchException {
+        List<NearbyPlace> nearbyPlaces = null;
+        if (ads != null && !ads.isEmpty()) {
+            nearbyPlaces = new ArrayList<NearbyPlace>();
+            for (PFPAd ad : ads) {
+                nearbyPlaces.add(NearbyPlacesHelper.toNearbyPlace(request, ad));
+            }
+            NearbyPlacesHelper.addDefaultImages(nearbyPlaces, contextPath);
+        }
+        return nearbyPlaces;
+    }
 
-	private List<NearbyPlace> toNearbyPlacesBackfill(RequestBean request,
-			List<PFPAd> ads) throws CitysearchException {
-		List<NearbyPlace> nearbyPlaces = null;
-		if (ads != null && !ads.isEmpty()) {
-			nearbyPlaces = new ArrayList<NearbyPlace>();
-			for (PFPAd ad : ads) {
-				nearbyPlaces.add(NearbyPlacesHelper.toBackfill(request, ad));
-			}
-			NearbyPlacesHelper.addDefaultImages(nearbyPlaces, contextPath);
-		}
-		return nearbyPlaces;
-	}
+    private List<NearbyPlace> toNearbyPlacesBackfill(RequestBean request, List<PFPAd> ads)
+            throws CitysearchException {
+        List<NearbyPlace> nearbyPlaces = null;
+        if (ads != null && !ads.isEmpty()) {
+            nearbyPlaces = new ArrayList<NearbyPlace>();
+            for (PFPAd ad : ads) {
+                nearbyPlaces.add(NearbyPlacesHelper.toBackfill(request, ad));
+            }
+            NearbyPlacesHelper.addDefaultImages(nearbyPlaces, contextPath);
+        }
+        return nearbyPlaces;
+    }
 
-	protected List<NearbyPlace> getAdsFromPFPLocation(RequestBean request)
-			throws CitysearchException {
-		log.info("NearbyPlacesHelper.getAdsFromPFPLocation: Begin");
-		PFPResponse pfpLocationResponse = pfpProxy.getAdsFromPFPLocation(
-				request, displaySize);
-		return toNearbyPlaces(request, pfpLocationResponse.getLocalPfp());
-	}
+    protected List<NearbyPlace> getAdsFromPFPLocation(RequestBean request)
+            throws CitysearchException {
+        log.info("NearbyPlacesHelper.getAdsFromPFPLocation: Begin");
+        PFPResponse pfpLocationResponse = pfpProxy.getAdsFromPFPLocation(request, displaySize);
+        return toNearbyPlaces(request, pfpLocationResponse.getLocalPfp());
+    }
 
-	protected List<NearbyPlace> getAdsFromPFP(RequestBean request,
-			Set<String> listingsToIgnore, int requiredNumberOfAds)
-			throws CitysearchException {
-		log.info("getAdsFromPFP: Begin");
-		PFPResponse pfpResponse = pfpProxy.getAdsFromPFP(request,
-				requiredNumberOfAds, listingsToIgnore);
-		return toNearbyPlaces(request, pfpResponse.getLocalPfp());
-	}
+    protected List<NearbyPlace> getAdsFromPFP(RequestBean request, Set<String> listingsToIgnore,
+            int requiredNumberOfAds) throws CitysearchException {
+        log.info("getAdsFromPFP: Begin");
+        if (listingsToIgnore == null) {
+            listingsToIgnore = new HashSet<String>();
+        }
+        PFPResponse pfpResponse = pfpProxy.getAdsFromPFP(request, requiredNumberOfAds,
+                listingsToIgnore);
+        return toNearbyPlaces(request, pfpResponse.getLocalPfp());
+    }
 
-	protected List<NearbyPlace> getNearbyPlacesBackfill(RequestBean request,
-			int noOfBackFillNeeded) throws CitysearchException {
-		PFPResponse response = pfpProxy.getBackFill(noOfBackFillNeeded);
-		List<NearbyPlace> backFillFromPFP = toNearbyPlacesBackfill(request,
-				response.getBackfill());
-		return backFillFromPFP;
-	}
+    protected List<NearbyPlace> getNearbyPlacesBackfill(RequestBean request, int noOfBackFillNeeded)
+            throws CitysearchException {
+        PFPResponse response = pfpProxy.getBackFill(noOfBackFillNeeded);
+        List<NearbyPlace> backFillFromPFP = toNearbyPlacesBackfill(request, response.getBackfill());
+        return backFillFromPFP;
+    }
 
-	protected List<NearbyPlace> getSearchResults(RequestBean request,
-			int maxNoOfResultsRequired) throws CitysearchException {
-		SearchHelper searchHelper = new SearchHelper(contextPath,
-				maxNoOfResultsRequired);
-		return searchHelper.getNearbyPlaces(request);
-	}
+    protected List<NearbyPlace> getSearchResults(RequestBean request, int maxNoOfResultsRequired)
+            throws CitysearchException {
+        SearchHelper searchHelper = new SearchHelper(contextPath, maxNoOfResultsRequired);
+        return searchHelper.getNearbyPlaces(request);
+    }
 }
