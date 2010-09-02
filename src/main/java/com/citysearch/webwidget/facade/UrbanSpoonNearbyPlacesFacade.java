c92976bc83bab75f11329901ff80a21a1ec0cc03
Aspert Benjamin <aspert@aspert-laptop.(none)>
2010-09-01 21:04:59 -0700

    Fix to avoid calling PFP location.

--- src/main/java/com/citysearch/webwidget/facade/UrbanSpoonNearbyPlacesFacade.java
+++ src/main/java/com/citysearch/webwidget/facade/UrbanSpoonNearbyPlacesFacade.java
@@ -1,11 +1,7 @@
 package com.citysearch.webwidget.facade;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.citysearch.webwidget.bean.HouseAd;
@@ -26,59 +22,39 @@ public class UrbanSpoonNearbyPlacesFacade extends AbstractNearByPlacesFacade {
     public NearbyPlacesResponse getNearbyPlaces(RequestBean request)
             throws InvalidRequestParametersException, CitysearchException {
         request.validate();
-        return findInsiderPagesNearbyPlaces(request);
+        return findNearbyPlaces(request);
     }
 
-    private NearbyPlacesResponse findInsiderPagesNearbyPlaces(RequestBean request)
+    private NearbyPlacesResponse findNearbyPlaces(RequestBean request)
             throws CitysearchException {
         NearbyPlacesResponse response = new NearbyPlacesResponse();
+        List<NearbyPlace> nearbyPlaces = getAdsFromPFP(request, null, getDisplaySize());
 
-        List<NearbyPlace> nearbyPlaces = null;
-
-        if (!StringUtils.isBlank(request.getLatitude())
-                && !StringUtils.isBlank(request.getLongitude())) {
-            nearbyPlaces = getAdsFromPFPLocation(request);
-        }
         List<NearbyPlace> backfill = null;
         List<HouseAd> houseAds = null;
         List<NearbyPlace> searchResults = null;
-        int noOfAdsNeeded = (nearbyPlaces == null || nearbyPlaces.isEmpty()) ? this.displaySize
+        int noOfBackFillNeeded = (nearbyPlaces == null || nearbyPlaces.isEmpty()) ? this.displaySize
                 : this.displaySize - nearbyPlaces.size();
-        if (noOfAdsNeeded > 0) {
-            nearbyPlaces = (nearbyPlaces == null) ? new ArrayList<NearbyPlace>() : nearbyPlaces;
-            if (!StringUtils.isBlank(request.getWhere())) {
-                Set<String> listingIds = new TreeSet<String>();
-                for (NearbyPlace nbp : nearbyPlaces)
-                    listingIds.add(nbp.getListingId());
-                List<NearbyPlace> pfpResults = getAdsFromPFP(request, listingIds, noOfAdsNeeded);
-                if (pfpResults != null && !pfpResults.isEmpty()) {
-                    pfpResults = (pfpResults.size() > noOfAdsNeeded) ? pfpResults.subList(0,
-                            noOfAdsNeeded) : pfpResults;
-                    nearbyPlaces.addAll(pfpResults);
+        if (noOfBackFillNeeded > 0) {
+            backfill = getNearbyPlacesBackfill(request, noOfBackFillNeeded);
+            int noOfSearchResultsNeeded = (backfill == null || backfill.isEmpty()) ? noOfBackFillNeeded
+                    : noOfBackFillNeeded - backfill.size();
+            if (noOfSearchResultsNeeded > 0) {
+                searchResults = getSearchResults(request, noOfSearchResultsNeeded);
+                int noOfHouseAdsNeeded = (searchResults == null || searchResults.isEmpty()) ? noOfSearchResultsNeeded
+                        : noOfSearchResultsNeeded - searchResults.size();
+                if (noOfHouseAdsNeeded > 0) {
+                    houseAds = HouseAdsUtil.getHouseAds(getContextPath(),
+                            request.getDartClickTrackUrl());
+                    houseAds = houseAds.subList(0, noOfHouseAdsNeeded);
+                } else if (noOfHouseAdsNeeded < 0) {
+                    searchResults = searchResults.subList(0, noOfSearchResultsNeeded);
                 }
-            }
-            int noOfBackFillNeeded = (nearbyPlaces == null || nearbyPlaces.isEmpty()) ? this.displaySize
-                    : this.displaySize - nearbyPlaces.size();
-            if (noOfBackFillNeeded > 0) {
-                backfill = getNearbyPlacesBackfill(request, noOfBackFillNeeded);
-                int noOfSearchResultsNeeded = (backfill == null || backfill.isEmpty()) ? noOfBackFillNeeded
-                        : noOfBackFillNeeded - backfill.size();
-                if (noOfSearchResultsNeeded > 0) {
-                    searchResults = getSearchResults(request, noOfSearchResultsNeeded);
-                    int noOfHouseAdsNeeded = (searchResults == null || searchResults.isEmpty()) ? noOfSearchResultsNeeded
-                            : noOfSearchResultsNeeded - searchResults.size();
-                    if (noOfHouseAdsNeeded > 0) {
-                        houseAds = HouseAdsUtil.getHouseAds(getContextPath(),
-                                request.getDartClickTrackUrl());
-                        houseAds = houseAds.subList(0, noOfHouseAdsNeeded);
-                    } else if (noOfHouseAdsNeeded < 0) {
-                        searchResults = searchResults.subList(0, noOfSearchResultsNeeded);
-                    }
-                } else if (noOfSearchResultsNeeded < 0) {
-                    backfill = backfill.subList(0, noOfBackFillNeeded);
-                }
+            } else if (noOfSearchResultsNeeded < 0) {
+                backfill = backfill.subList(0, noOfBackFillNeeded);
             }
         }
+
         response.setNearbyPlaces(nearbyPlaces);
         response.setBackfill(backfill);
         response.setSearchResults(searchResults);
